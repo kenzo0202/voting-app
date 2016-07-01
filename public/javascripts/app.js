5afc4ba3c9900d08fd7b2b409fe751eac5203023
tyk2013 <ken.rad0202@gmail.com>
2016-07-01 15:44:54 +0900

    first-commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kenzo on 2016/07/01.
+ */
+angular.module('polling', [])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+        when('/polls', { templateUrl: 'partials/list.html', controller:
+        PollListCtrl }).
+        when('/poll/:pollId', { templateUrl: 'partials/item.html', controller:
+        PollItemCtrl }).
+        when('/new', { templateUrl: 'partials/new.html', controller:
+        PollNewCtrl }).
+        otherwise({ redirectTo: '/polls' });
+    }]);
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,27 @@
+/**
+ * Created by kenzo on 2016/07/01.
+ */
+function PollListCtrl($scope) {
+    $scope.polls = [];
+}
+
+function PollItemCtrl($scope,$routeParams) {
+    $scope.poll = {};
+    $scope.vote = function () {
+        
+    }
+
+}
+
+function PollNewCtrl($scope) {
+    $scope.poll = {
+        question: '',
+        choices:[{text: ''},{text: ''},{text: ''}]
+    };
+    $scope.addChoice = function () {
+        $scope.poll.choices.push({text: ''});
+    };
+    $scope.createPoll = function () {
+        
+    };
+}
